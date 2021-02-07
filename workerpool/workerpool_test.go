aaf23f3f2647148ac7b3371dff2a524fb8d1d09b
Aaron LI <aly@aaronly.me>
2021-02-07 21:12:58 +0800

    workerpool: Update and add a new test

--- workerpool/workerpool_test.go
+++ workerpool/workerpool_test.go
@@ -1,20 +1,48 @@
 package workerpool
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 
 // Credit: https://brandur.org/go-worker-pool
-func TestWorkerPool(t *testing.T) {
+func TestWorkerPool1(t *testing.T) {
 	tasks := []*Task{
-		NewTask(func() error { return nil }),
-		NewTask(func() error { return nil }),
-		NewTask(func() error { return nil }),
+		NewTask(func(data interface{}) error { return nil }, nil),
+		NewTask(func(data interface{}) error { return nil }, nil),
+		NewTask(func(data interface{}) error { return nil }, nil),
 	}
 
 	p := NewPool(tasks, 3)
 	p.Run()
 
 	for _, task := range p.Tasks {
+		if task.Err != nil {
+			t.Error(task.Err)
+		}
+	}
+}
+
+
+// https://hackernoon.com/concurrency-in-golang-and-workerpool-part-2-l3w31q7
+func TestWorkerPool2(t *testing.T) {
+	var allTask []*Task
+	for i := 0; i < 100; i++ {
+		task := NewTask(func(data interface{}) error {
+			taskID := data.(int)
+			time.Sleep(100 * time.Millisecond)
+			fmt.Printf("Task %d processed\n", taskID)
+			return nil
+		}, i)
+		allTask = append(allTask, task)
+	}
+
+	pool := NewPool(allTask, 5)
+	pool.Run()
+
+	for _, task := range pool.Tasks {
 		if task.Err != nil {
 			t.Error(task.Err)
 		}
