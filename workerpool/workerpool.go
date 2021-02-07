00e27322fd94da48cd41d6efdf20b3e406887102
Aaron LI <aly@aaronly.me>
2021-02-07 21:45:32 +0800

    workerpool: Support background workers
    Credit: https://hackernoon.com/concurrency-in-golang-and-workerpool-part-2-l3w31q7

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -37,15 +37,18 @@ type Worker struct {
 	ID	int
 
 	tasks	chan *Task
+	quit	chan bool
 }
 
 func NewWorker(id int, tasks chan *Task) *Worker {
 	return &Worker{
 		ID: id,
 		tasks: tasks,
+		quit: make(chan bool),
 	}
 }
 
+// Start the worker and do the tasks.
 func (w *Worker) Start(wg *sync.WaitGroup) {
 	wg.Add(1)
 	go func() {
@@ -56,14 +59,35 @@ func (w *Worker) Start(wg *sync.WaitGroup) {
 	}()
 }
 
+// Start the worker in background, waiting for tasks.
+func (w *Worker) StartBackground() {
+	for {
+		select {
+		case task := <-w.tasks:
+			task.Run()
+		case <-w.quit:
+			return
+		}
+	}
+}
+
+// Stop the background worker.
+func (w *Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
+
 
 // A worker group that runs tasks at a configured concurrency.
 //
 type Pool struct {
 	Tasks		[]*Task
+	Workers		[]*Worker
 
 	concurrency	int
 	collector	chan *Task
+	quit		chan bool
 	wg		sync.WaitGroup
 }
 
@@ -76,7 +100,6 @@ func NewPool(tasks []*Task, concurrency int) *Pool {
 }
 
 // Run all tasks in the pool and blocks until it's finished.
-//
 func (p *Pool) Run() {
 	for i := 0; i < p.concurrency; i++ {
 		worker := NewWorker(i, p.collector)
@@ -90,4 +113,33 @@ func (p *Pool) Run() {
 	close(p.collector)
 
 	p.wg.Wait()
+}
+
+// Add a task to the pool.
+func (p *Pool) AddTask(task *Task) {
+	p.collector <- task
+}
+
+// Run the workers in background.
+func (p *Pool) RunBackground() {
+	for i := 0; i < p.concurrency; i++ {
+		worker := NewWorker(i, p.collector)
+		p.Workers = append(p.Workers, worker)
+		go worker.StartBackground()
+	}
+
+	for i := range p.Tasks {
+		p.collector <- p.Tasks[i]
+	}
+
+	p.quit = make(chan bool)
+	<-p.quit
+}
+
+// Stop the background workers.
+func (p *Pool) Stop() {
+	for i := range p.Workers {
+		p.Workers[i].Stop()
+	}
+	p.quit <- true
 }
--- workerpool/workerpool_test.go
+++ workerpool/workerpool_test.go
@@ -2,6 +2,7 @@ package workerpool
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 )
@@ -41,6 +42,40 @@ func TestWorkerPool2(t *testing.T) {
 
 	pool := NewPool(allTask, 5)
 	pool.Run()
+
+	for _, task := range pool.Tasks {
+		if task.Err != nil {
+			t.Error(task.Err)
+		}
+	}
+}
+
+func TestWorkerPool3(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	var allTask []*Task
+	pool := NewPool(allTask, 5)
+
+	go func() {
+		for {
+			taskID := rand.Intn(100) + 20
+
+			if taskID % 7 == 0 {
+				pool.Stop()
+			}
+
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+			task := NewTask(func(data interface{}) error {
+				taskID := data.(int)
+				time.Sleep(100 * time.Millisecond)
+				fmt.Printf("Task %d processed\n", taskID)
+				return nil
+			}, taskID)
+			pool.AddTask(task)
+		}
+	}()
+
+	pool.RunBackground()
 
 	for _, task := range pool.Tasks {
 		if task.Err != nil {
